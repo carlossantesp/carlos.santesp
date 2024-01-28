6eeb82d3444acf116f7b5b0dc618d01c781c95e8
Carlos Santillan <carlos.santesp@gmail.com>
2024-01-27 22:52:07 -0400

    Adding footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,7 +1,17 @@
+import Container from "./container";
+
 export default function Footer() {
     return (
-        <footer>
-            <p>Desing and Build by Carlos Santillan - 2024</p>
-        </footer>
+        <>
+            <hr className="opacity-5 max-w-2xl md:max-w-5xl mx-auto" />
+            <footer className="m-6">
+                <Container>
+                    <p className="text-center text-sm text-gray-400">
+                        &copy; Desing and Build by <span className="text-sky-400">Carlos Santillan</span> -{" "}
+                        {new Date().getFullYear()}
+                    </p>
+                </Container>
+            </footer>
+        </>
     );
 }
