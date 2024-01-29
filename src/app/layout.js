e605b463aa4d0b3ac31a8d979432a02885348009
Carlos Santillan <carlos.santesp@gmail.com>
2024-01-29 19:39:28 -0400

    Fixing metadata images

--- src/app/layout.js
+++ src/app/layout.js
@@ -16,12 +16,12 @@ export const metadata = {
         siteName: "Carlos Santillan - Front-End Developer",
         images: [
             {
-                url: "https://dev-carlos-santillan.netlify.app/page.png",
+                url: "https://dev-carlos-santillan.netlify.app/images/page.png",
                 width: 800,
                 height: 600,
             },
             {
-                url: "https://dev-carlos-santillan.netlify.app/page-alt.png",
+                url: "https://dev-carlos-santillan.netlify.app/images/page-alt.png",
                 width: 1800,
                 height: 1600,
                 alt: "Screen portfolio web",
